c6af6d5299986b2802e974fac00814ee0ced246f
Mark Meves <markmeves@mark-mevess-macbook-pro.local>
2009-11-27 03:28:44 -0500

    about to change logic for shorthands to use instance_eval instead of method_missing

--- lib/hipe-gorillagrammar.rb
+++ lib/hipe-gorillagrammar.rb
@@ -2,21 +2,9 @@ require 'rubygems'
 require 'ruby-debug'
 require 'singleton'
 
-module Hipe; module GorillaGrammar; module PipeHack; end; end; end;
-  
 class Symbol 
-  include Hipe::GorillaGrammar::PipeHack
   def satisfied?; @is_satisfied; end
   def accepting?; @is_accepting; end
-  # we might regret this
-  def =~ (symbol_data)
-    Hipe::GorillaGrammar::Runtime.instance.current_grammar!.register_symbol self, symbol_data
-  end
-  alias_method :[], :=~ # allows for e.g. a terminal 'var' like  :first_name[/^.+$/]
-end
-
-class String  # experimental
-  include Hipe::GorillaGrammar::PipeHack
 end
 
 :D.instance_variable_set :@is_satisfied, true 
@@ -32,8 +20,8 @@ end
 
 module Hipe
   
-  def self.GorillaGrammar *args, &block
-    GorillaGrammar.define(*args, &block)
+  def self.GorillaGrammar opts=nil, &block
+    GorillaGrammar.define(opts, &block)
   end
   
   module GorillaGrammar
@@ -41,11 +29,12 @@ module Hipe
     VERSION = '0.0.0'
     Infinity = 1.0 / 0
     
-    def self.define(name = nil, &block)
+    def self.define(opts=nil, &block)
+      g = Runtime.instance.create_grammar! opts      
+      Runtime.instance.push_grammar g      
       begin
-        g = Runtime.instance.create_grammar! name
-        Runtime.instance.push_grammar g
-        g.instance_eval(&block)
+
+        g.define(&block)
       ensure
         Runtime.instance.pop_grammar        
       end
@@ -71,21 +60,51 @@ module Hipe
         return instance.send(a,*b) if instance.respond_to? a
         raise NoMethodError.new %{undefined method `#{a}' for #{inspect}}
       end
-      def create_grammar! name
-        raise GorillaException.new("for now we can't reopen grammars") if name && @grammars[name]
-        g = Grammar.new
-        name ||= %{grammar#{g.object_id}}
-        g.name = name
-        @grammars[name] = g
+      def create_grammar! opts
+        opts ||= {}
+        raise GorillaException.new("for now we can't reopen grammars") if opts[:name] && @grammars[opts[:name]]
+        g = Grammar.new opts
+        @grammars[g.name] = g
+      end
+      def get_grammar name; @grammars[name]; end
+      # enables the use of  =~,  ||,  (0..1).of .., .., .., and :some_name[/regexp/] in grammars
+      def enable_operator_shorthands
+        return if @shorthands_enabled
+        Symbol.instance_eval do 
+          include PipeHack          
+          define_method :=~ do |symbol_data|
+            return super unless Runtime.instance.current_grammar
+            Runtime.instance.current_grammar![self] = symbol_data            
+          end
+          # this is functionally equivalent to the above but w/ a different error message
+          define_method :[] do |symbol_data|
+            return super unless Runtime.instance.current_grammar            
+            Runtime.instance.current_grammar![self] = symbol_data
+          end
+          # alias_method :[], :=~ # allows for e.g. a terminal 'var' like  :first_name[/^.+$/]          
+        end
+        String.instance_eval { include PipeHack }
+        Fixnum.instance_eval { include FixnumOfHack }
+        Range.instance_eval { include RangeOfHack }
+        @shorthands_enabled = true     
       end
     end
-    
-    class Grammar
+    class Grammar < Hash
       @shorthands = {}
       class << self
         attr_reader :shorthands
       end
       attr_accessor :name
+      def initialize opts
+        opts ||= {}
+        @name = opts[:name] || %{grammar#{self.object_id}}
+        @with_operator_shorthands = opts.has_key?(:enable_operator_shorthands) ?    
+          opts[:enable_operator_shorthands] : true
+      end
+      def define &block
+        Runtime.enable_operator_shorthands if @with_operator_shorthands
+        self.instance_eval(&block)
+      end
       def self.register_shorthand name, klass
         @shorthands[name] = klass
       end
@@ -93,13 +112,19 @@ module Hipe
         if Grammar.shorthands[name]
           Grammar.shorthands[name].construct_from_shorthand name, *args
         else
-          super name, *args
-          #s = 'available shorthands: ('+Grammar.shorthands.keys.map{|k| k.to_s}.sort * ', '+')'
-          #raise NoMethodError.new "undefined method #{name} for #{self.inspect}:#{self.class} -- #{s}"
+          #super name, *args
+          s = 'available shorthands: ('+Grammar.shorthands.keys.map{|k| k.to_s}.sort * ', '+')'
+          raise NoMethodError.new "undefined method #{name} for #{self.inspect}:#{self.class} -- #{s}"
         end
       end
+      def []= name, symbol_data
+        raise UsageFailure.new(%{Can't redefine symbols (#{name})}) if self[name]        
+        symbol = SymbolSet.bless symbol_data
+        super name, symbol_data
+      end
+      #def [] name
+      #end
     end
-    
     class GorillaException < Exception
       def initialize(*args)
         super args.shift if args[0].instance_of? String  
@@ -128,34 +153,42 @@ module Hipe
       EOS
       end
     end
-
     module PipeHack
       def |(other)
-        ok = case other.class; when String; true; when Symbol; true; else; false; end
-        raise UsageFailure("for now, pipe hack on strings and symbols only") unless ok
         if self.instance_of? RangeOf
           raise UsageFailure("no") unless self.pipe_hack
-          self.unshift other
+          self << other
           ret = self
         else 
-          ret = RangeOf.new((1..1),other)
+          ret = RangeOf.new((1..1),[self,other])
           ret.pipe_hack = true
         end
         ret
       end
     end
-    
+    module FixnumOfHack # note that IRL, this will rarely be used for other than 1
+      def of *args
+        return RangeOf.new((1..1), args)
+      end
+    end
+    module RangeOfHack
+      def of *args
+        return RangeOf.new(self, args)
+      end
+    end
     # This guy is the one that manages mapping builtin ruby data
     # structures to classes of symbols in our grammar thing.
     module SymbolSet # "bless" an array of symbol ruby structures
       # @return the original object extended or a new object
       def self.bless obj 
         case obj
-          when GorillaSymbol then obj
-          when Symbol        then GrammarSymbol.new obj  
+          when GorillaSymbol then obj # keep this one first!          
+          when Array         then Sequence.new(*obj)          
+          when Symbol        then obj.extend SymbolReference
           when String        then obj.extend StringTerminal
           when Regexp        then obj.extend RegexpTerminal
-          else UsageFailure.new %{don't know what to do with "#{obj.inspect}"}
+          else
+            raise UsageFailure.new %{Can't determine symbol type for "#{obj.inspect}"},:obj=>obj
         end  
       end
       # remember this may be used for both lists and sets
@@ -185,13 +218,6 @@ module Hipe
         ret.init_for_parse if ret.respond_to? :init_for_parse
         ret
       end
-      def to_native
-        self.dup
-      end
-      #def inspect
-      #  %{#{super}#{self.class.ancestors.map{|m| m.to_s}.select{|s|/^Hipe::GorillaGrammar/=~s}.join(',')}}
-      #end
-      # def expecting -- return array of objects that respond to to_s
     end
     
     module TerminalSymbol # @abstract base
@@ -215,11 +241,15 @@ module Hipe
       def expecting; [%{"#{self}"}]; end
     end
     
-    module GrammarSymbol
+    module SymbolReference
       include GorillaSymbol
-      def self.new symbol
-        super symbol
+      def copy_for_parse
+        grammar.get_symbol(self).copy_for_parse
       end
+      def self.extend_object symbol
+        @grammar = Runtime.current_grammar!
+        @grammar_name = @grammar.name # we store name maybe for marshalling
+      end 
     end
 
     module RegexpTerminal
@@ -274,34 +304,39 @@ module Hipe
       include CanParse, NonTerminalSymbol
       extend GorillaSymbol::ModuleMethods 
       register_shorthand_constructor :sequence, self
-
       def self.construct_from_shorthand name, *args
-        self.new name, *args
+        self.new(*args)
       end
-      def initialize name, *args
+      def initialize *args
         raise GrammarGrammarException.new "Arguments must be non-zero length" unless args.size > 0
-        @grammar = args
-        @grammar.extend SymbolSet
+        @group = args
+        @group.extend SymbolSet
       end
+      def inspect
+        return super if @is_parse
+        return @group.inspect
+      end
+      def to_s; inspect; end
       def init_for_parse
         @index = 0
+        @is_parse = true
       end
       def _advance
         self << @current_child
         @current_child = nil
         @index += 1
-        if @index == @grammar.size
-          @grammar = nil
+        if @index == @group.size
+          @group = nil
           :>
         else
           :O
         end        
       end
       def expecting
-        curr = @grammar[@index]
+        curr = @group[@index]
         ret = curr.expecting
-        if curr.kind_of? RangeOf and curr.range.begin == 0 and @index < @grammar.size
-          ret |= @grammar[@index+1].expecting
+        if curr.kind_of? RangeOf and curr.range.begin == 0 and @index < @group.size
+          ret |= @group[@index+1].expecting
         end
         ret
       end
@@ -311,7 +346,7 @@ module Hipe
           if (@current_child)
             prev_child_status = @current_child.status
           else          
-            @current_child = @grammar[@index].copy_for_parse
+            @current_child = @group[@index].copy_for_parse
             prev_child_status = nil
           end
           child_status = @current_child.match token, peek
@@ -331,7 +366,7 @@ module Hipe
               end
             when :O then :O
             when :D 
-              (@index==@grammar.size-1) ? :D : :O
+              (@index==@group.size-1) ? :D : :O
             else 
               raise GorillaException.new('symbol returned bad status')
           end # case
@@ -339,7 +374,6 @@ module Hipe
         end while true
         @status = status
       end # def match
-      
       def initial_status; :O; end       
       def to_native
         self.map do |x|
@@ -351,35 +385,52 @@ module Hipe
     class RangeOf < Array
       include CanParse, NonTerminalSymbol, PipeHack
       extend GorillaSymbol::ModuleMethods
-      register_shorthand_constructor :zero_or_more_of, self      
+      register_shorthand_constructor :zero_or_more_of, self
       register_shorthand_constructor :one_or_more_of, self
       register_shorthand_constructor :zero_or_one_of, self
-      register_shorthand_constructor :one_of, self      
+      register_shorthand_constructor :one_of, self
+      register_shorthand_constructor :range_of, self
       def self.construct_from_shorthand name, *args
-        self.new name, *args
+        self.new name, args
       end      
-      protected 
-      def initialize name, *args
+      def initialize name, args
         raise GrammarGrammarException.new "Arguments must be non-zero length" unless args.size > 0
         @range = name.instance_of?(Range) ? name : case name
           when :zero_or_more_of then (0..Infinity)
           when :one_or_more_of then (1..Infinity)
           when :zero_or_one_of then (0..1)
           when :one_of then (1..1)
+          when :range_of then @range = args.shift
           else raise UsageFailure.new(%{invalid name string "#{name}"})
         end
+        raise UsageFailure.new("must be range") unless @range.instance_of? Range
         @group = args
         @group.extend SymbolSet
       end
-      def unshift jobber # for PipeHack
-        raise GorillaException.new('no') if @is_parse
-        @grammar.unshift jobber
+      def children # for debugging from ir
+        @group
       end
-      public
+      def << jobber # for PipeHack. code smell @ note 1
+        if @is_parse 
+          super.<<(jobber)
+        else  
+          @group << SymbolSet.bless(jobber)
+        end
+      end
       attr_reader :range
       attr_accessor :pipe_hack
       def expecting
         @group.map{|x| x.expecting}.flatten
+      end
+      def inspect
+        return super if @is_parse
+        return %{(#{@range.inspect}):#{@group.inspect}}
+      end
+      def to_s; inspect; end
+      def pretty_print(q)
+        q.group 1, %{(#{range}).of }, '' do
+          q.pp @group
+        end
       end
       def init_for_parse
         @is_parse = 1
@@ -443,7 +494,6 @@ module Hipe
           x.to_native
         end
       end
-      
       ## @fixme this is waiting for unparse()
       def self.join list, conj1, conj2, &block
         list.map!(&block) if block
